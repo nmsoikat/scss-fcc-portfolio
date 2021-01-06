6186a5649b3c6bbfdbbf4cb8ceaab74fab2e1fec
unknown <nursaykat@gmail.com>
2021-01-06 18:05:57 +0600

    home page done

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  const menuBtn = document.querySelector(".menu-btn")
+  const burger = document.querySelector(".menu-btn__burger")
+  const nav = document.querySelector(".nav")
+  const menuNav = document.querySelector(".menu-nav")
+  const menuNavItem = document.querySelectorAll(".menu-nav__item")
+
+  menuBtn.addEventListener("click", toggler)
+
+  let showMenu = false
+  function toggler() {
+    if (!showMenu) {
+      burger.classList.add("open")
+      nav.classList.add("open")
+      menuNav.classList.add("open")
+      menuNavItem.forEach((item) => item.classList.add("open"))
+
+      showMenu = true
+    } else {
+      burger.classList.remove("open")
+      nav.classList.remove("open")
+      menuNav.classList.remove("open")
+      menuNavItem.forEach((item) => item.classList.remove("open"))
+
+      showMenu = false
+    }
+  }
+}
